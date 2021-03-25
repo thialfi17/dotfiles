9af19cb2e471ae1895b55ad3f5c3308629b89860
thialfi17 <thialfi17@gmail.com>
2021-03-25 01:00:23 +0000

    Changed to use normal standard of 'o' for global

--- .config/nvim/lua/myconf/settings.lua
+++ .config/nvim/lua/myconf/settings.lua
@@ -1,39 +1,39 @@
-local scopes = {g = vim.o, b = vim.bo, w = vim.wo}
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
 local function opt(scope, key, value)
 	scopes[scope][key] = value
-	if scope ~= 'g' then
-		scopes['g'][key] = value
+	if scope ~= 'o' then
+		scopes['o'][key] = value
 	end
 end
 
 vim.cmd('syntax on')
 
 -- Global Options
-opt('g', 'hidden', true)
-opt('g', 'scrolloff', 10)
+opt('o', 'hidden', true)
+opt('o', 'scrolloff', 10)
 --o.ruler = true
 --o.hlsearch = true
 --o.wildmenu = true
-opt('g', 'wildmode', 'longest:full,full')
-opt('g', 'history', 1000)
+opt('o', 'wildmode', 'longest:full,full')
+opt('o', 'history', 1000)
 --o.laststatus = 2
 --o.backspace = 'indent,start,eol'
 --o.showcmd = true
-opt('g', 'foldlevelstart', 99)
-opt('g', 'splitright', true)
-opt('g', 'splitbelow', true)
-opt('g', 'mouse', 'a')
+opt('o', 'foldlevelstart', 99)
+opt('o', 'splitright', true)
+opt('o', 'splitbelow', true)
+opt('o', 'mouse', 'a')
 --o.background = 'dark'
-opt('g', 'showbreak', ' ↳')
-opt('g', 'listchars', 'tab:→ ,nbsp:⍽,trail:␣,extends:⇒,precedes:⇐')
+opt('o', 'showbreak', ' ↳')
+opt('o', 'listchars', 'tab:→ ,nbsp:⍽,trail:␣,extends:⇒,precedes:⇐')
+opt('o', 'completeopt', 'menuone,noselect')
 
 
 -- Window Options
 opt('w', 'number', true)
 opt('w', 'cursorcolumn', true)
 opt('w', 'wrap', true)
-opt('w', 'foldcolumn', '1')
 opt('w', 'foldmethod', 'expr')
 opt('w', 'foldexpr', 'nvim_treesitter#foldexpr()')
 opt('w', 'foldenable', true)
