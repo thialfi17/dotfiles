a8ded983dbffcc43e61e0ba64e450233e4e6d2dc
thialfi17 <thialfi17@gmail.com>
2024-02-11 23:48:45 +0000

    Further updates

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -3,7 +3,7 @@
 -- Plugin Management: {{{
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.uv.fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
