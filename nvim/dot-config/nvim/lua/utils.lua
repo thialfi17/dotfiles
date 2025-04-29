308c4f61ca283919075c5ad17c6706807bb1b8ae
thialfi17 <thialfi17@gmail.com>
2025-04-29 23:37:00 +0100

    nvim: Added cmdline parsing util

--- nvim/dot-config/nvim/lua/utils.lua
+++ nvim/dot-config/nvim/lua/utils.lua
@@ -1,5 +1,77 @@
 local M = {}
 
+--- Function to parse args passed to a user command and split them based on
+--- quotes
+---@param cmd string Command as received on the commandline
+---@return string[] parts Command split into reasonable chunks based on whitespace and quotes
+M.split_command = function (cmd)
+    -- Output chunks
+    local parts = {}
+    -- Which output chunk
+    local i = 1
+    -- Previous delimiter
+    local delim = ""
+    -- Previous char
+    local p = ""
+
+    for c in cmd:gmatch(".") do
+        -- Ensure there is somewhere to put chars in new chunk
+        if #parts < i then
+            parts[i] = {}
+        end
+
+        -- if not escaping and (closing chunk or opening new chunk (delim or space))
+        if p ~= "\\" and (c == delim or (delim == "" and (c == '"' or c == '`' or c == "'" or c == " "))) then
+            local inc = 0
+
+            -- If chunk is not empty then we must be moving to to a new chunk
+            -- this could be due to a space so delim may not be set
+            if #(parts[i]) ~= 0 then
+                inc = 1
+            end
+
+            if c == delim then -- closing delimiter
+                delim = ""
+                -- TODO: At some point this was included so presumably it was
+                -- needed, but it breaks when used with command running.
+                --
+                -- Add delimiter to output
+                --table.insert(parts[i], c)
+            else -- opening new chunk due to delim or space
+                if c ~= " " then
+                    delim = c
+                end
+
+                -- inc would be zero due to quotes after space
+                if inc > 0 then
+                    table.insert(parts, {})
+                end
+
+                -- TODO: At some point this was included so presumably it was
+                -- needed, but it breaks when used with command running.
+                --
+                -- Add delimiter to output but not spaces
+                --if delim ~= "" then
+                --    table.insert(parts[i+inc], c)
+                --end
+            end
+            i = i + inc
+        else
+            table.insert(parts[i], c)
+        end
+        p = c
+    end
+
+    -- Do all concats in one go to avoid lots of individual calls. Supposedly
+    -- this is better for performance.
+    local res = {}
+    for _, v in ipairs(parts) do
+        table.insert(res, table.concat(v))
+    end
+
+    return res
+end
+
 ---@param path string Path with optional wildcards to scan and generate a list of paths
 ---@param filter? fun(name: string, type: string):boolean Function used to filter path results based on item path and type
 ---@return nil|fun(): string
